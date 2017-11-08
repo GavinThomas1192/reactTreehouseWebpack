e577307bc713a40c3c1bf0563677d3cab868452b
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-08 09:38:16 -0800

    Minified bundle for production

--- setting-up-a-react-app-with-webpack/video-5/scoreboard_final/webpack.config.js
+++ setting-up-a-react-app-with-webpack/video-5/scoreboard_final/webpack.config.js
@@ -1,23 +1,26 @@
 
 module.exports = {
+  // devtool: 'source-map',
+
   entry: './app.js',
-  
+
   output: {
-    // path: 'build',
+    path: 'build',
     filename: 'bundle.js'
   },
-  
+  // console.log('inside webpack');
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader'] 
+        loaders: ['react-hot', 'babel-loader']
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader' 
+        loader: 'style-loader!css-loader'
       }
     ]
   }
-};
+}
+
