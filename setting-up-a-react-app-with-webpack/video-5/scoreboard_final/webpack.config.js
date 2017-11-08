1fcbd440e7aa4cf6b750cbd1b9663f5dd5eaa001
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-08 12:45:46 -0800

    deleted

--- setting-up-a-react-app-with-webpack/video-5/scoreboard_final/webpack.config.js
+++ setting-up-a-react-app-with-webpack/video-5/scoreboard_final/webpack.config.js
@@ -1,6 +1,15 @@
-
+const webpack = require('webpack')
 module.exports = {
   devtool: 'source-map',
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
 
   entry: './app.js',
 
